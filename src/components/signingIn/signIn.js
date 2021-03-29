61212b0f36f28d99071792f4aef973ae0425ac36
AmmanMeb <72285018+AmmanMeb@users.noreply.github.com>
2021-03-29 09:55:49 -0400

    Update signIn.js

--- src/components/signingIn/signIn.js
+++ src/components/signingIn/signIn.js
@@ -1,7 +1,6 @@
 import apiClient from '../../api/apiclient'
 import React, { useState } from 'react';
 import {Link} from "react-router-dom";
-import {Redirect} from 'react-router-dom';
 import { Grid, Button, TextField } from "@material-ui/core"
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -24,59 +23,52 @@ function SignIn() {
         
     const [username, getUser] = useState ('')
     const [password, getPass] = useState ('')
-    
-    const onSubmit = (event) => {
-     event.preventDefault()
-      apiClient.signIn(username, password) .then( (response) => {
-        console.log(response)
 
-        if (response === 'Logged In') {          
-        return <Redirect to='/forum' />
-      }
-      else {
-        return document.getElementById('error').textContent = "Wrong Username or Password"
-      }
-      })
-
-      .catch(error => {
-        console.log('Error found when creating meeting');
-      })
-
-      
+const onSubmit = (event) => {
+  event.preventDefault()
+  apiClient.signIn(username, password).then((response) => {
+    console.log(response)  
+    if (response === 'Logged In') {
+      return  document.getElementById('error').innerHTML = ("Correct Username or Password"); /*<Redirect to='/forum' />*/
     }
-    
-       
-      return (
-        <section class="index-banner">
-          <ThemeProvider theme={theme}>
-            <Grid container style = {{minHeight: '100vh'}} spacing={0}  direction="column"  alignItems="center">
-              <Grid item xs={12} sm={12}>
+    else {
+      document.getElementById('error').innerHTML = ("Wrong Username or Password");
+    }
+  }).catch(error => {
+    console.log('Error found when creating meeting');
+  })
+}
 
-                <div className = "title-2">Sign In</div>
-                <div>
-                  <FormControl margin = "normal">
-                    <TextField label="Username" value = {username} onChange={e => { getUser(e.target.value) }} spacing={4} />    
-                    <TextField label="Password" value = {password} onChange={e => { getPass(e.target.value) }} spacing={4}/>
-                   
-                  </FormControl>
-                </div>                 
-                <Grid item xs={12} sm={12}>
-                  <Button onClick={onSubmit} variant="contained" component="span" spacing={4} color = "secondary">
-                    Sign In
-                  </Button>
-               </Grid> 
-               <Grid item xs={12} sm={12}>
-                  <Button component = {Link} to = "/" spacing={4} color = "primary">
-                     Create Account
-                  </Button>
-                   <div className="error"></div>
-               </Grid>
-              </Grid>
-            </Grid>
-          </ThemeProvider>      
-        </section>         
-      )      
-       
+return (
+  <section class="index-banner">
+    <ThemeProvider theme={theme}>
+      <Grid container style = {{minHeight: '100vh'}} spacing={0}  direction="column"  alignItems="center">
+        <Grid item xs={12} sm={12}>
+        
+          <div className = "title-2">Sign In</div>
+          <div>
+            <FormControl margin = "normal">
+              <TextField label="Username" value = {username} onChange={e => { getUser(e.target.value) }} spacing={4} />    
+              <TextField label="Password" value = {password} onChange={e => { getPass(e.target.value) }} spacing={4}/>
+              
+            </FormControl>
+          </div>                 
+          <Grid item xs={12} sm={12}>
+            <Button onClick={onSubmit} variant="contained" component="span" spacing={4} color = "secondary">
+              Sign In
+            </Button>
+          </Grid> 
+          <Grid item xs={12} sm={12}>
+            <Button component = {Link} to = "/" spacing={4} color = "primary">
+              Create Account
+            </Button>
+            <div id="error"></div>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ThemeProvider>      
+  </section>         
+) 
        
      }
      export default SignIn;
